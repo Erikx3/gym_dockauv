8e612683fe6c91e5b27f1859363c6dd2ec87fbfe
Erik Suer <eriksuer@web.de>
2022-04-05 23:51:28 +0200

    Add first sphere object with plotting variables

---
+++ gym_dockauv/objects/shape.py
@@ -0,0 +1,45 @@
+import numpy as np
+from abc import ABC, abstractmethod
+from functools import cached_property
+
+from typing import List
+
+
+class Shape(ABC):
+    """
+    This is a base class for any shape, should always contain coordinates of position.
+    """
+
+    def __init__(self, position: np.ndarray):
+        self.position = np.array(position)
+
+    @abstractmethod
+    def plot_variables(self) -> List[np.ndarray]:
+        """
+        Function that returns the plot variables for the matplotlib axes.surface_plot function
+
+        :return: return list of 1d arrays for plotting
+        """
+        pass
+
+
+class Sphere(Shape):
+
+    def __init__(self, position: np.ndarray, radius: float):
+        super().__init__(position)  # Call inherited init functions and then add to it
+        self.radius = radius
+
+    @abstractmethod
+    def plot_variables(self):
+        x_c, y_c, z_c = self.plot_shape
+        return [self.position[0] + x_c,
+                self.position[1] + y_c,
+                self.position[2] + z_c]
+
+    @cached_property
+    def plot_shape(self):
+        u, v = np.mgrid[0:2 * np.pi:20j, 0:np.pi:10j]
+        x_c = self.radius * np.cos(u) * np.sin(v)
+        y_c = self.radius * np.sin(u) * np.sin(v)
+        z_c = self.radius * np.cos(v)
+        return x_c, y_c, z_c
